ab99cebe29bda390cb034a81f8dbe36129baa727
Amra Ibrahimovic <amra.ibrahimovic@snhu.edu>
2022-11-03 17:05:15 -0400

    Add functionality to main menu buttons

--- app/src/main/java/com/example/productivityapp/MainActivity.java
+++ app/src/main/java/com/example/productivityapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.productivityapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,6 +15,8 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.productivityapp.databinding.ActivityMainBinding;
 
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
@@ -22,6 +27,25 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Button buttonS = findViewById(R.id.settings);
+        buttonS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SettingsScreen.class);
+                startActivity(intent);
+
+            }
+        });
+
+        Button buttonI = findViewById(R.id.inventory);
+        buttonI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,InventoryScreen.class);
+                startActivity(intent);
+            }
+        });
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
---
+++ app/src/main/java/com/example/productivityapp/SettingsScreen.java
@@ -0,0 +1,14 @@
+package com.example.productivityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_screen);
+    }
+}
