d7a9325af88c61a43e309053fbbe4fb07d7824e8
Owlss <eerinmacdonaldd@gmail.com>
2022-11-13 14:18:09 -0500

    added prototype inventory screen with no functionalty

--- app/src/main/java/com/example/productivityapp/InventoryScreen.java
+++ app/src/main/java/com/example/productivityapp/InventoryScreen.java
@@ -2,13 +2,67 @@ package com.example.productivityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.productivityapp.databinding.ActivityMainBinding;
 
 public class InventoryScreen extends AppCompatActivity {
+    Boolean[] owned = new Boolean[6];
+    int selection = 0;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory_screen);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Button base = findViewById(R.id.base_button);
+        base.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button swim = findViewById(R.id.swim_button);
+        swim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button doc = findViewById(R.id.doc_button);
+        doc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button gang = findViewById(R.id.gang_button);
+        gang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+        Button nes = findViewById(R.id.nes_button);
+        nes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+        Button school = findViewById(R.id.school_button);
+        school.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/productivityapp/MainActivity.java
+++ app/src/main/java/com/example/productivityapp/MainActivity.java
@@ -12,10 +12,16 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.example.productivityapp.databinding.ActivityMainBinding;
 
 import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -56,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-    }
 
+        }
 }
+
