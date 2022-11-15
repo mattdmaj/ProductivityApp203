0e2df1416135f934f672a7604fdfe20152b69c99
Paul Lamy <lamywmpaul@gmail.com>
2022-11-15 18:04:53 -0500

    updated Points file

--- app/src/main/java/com/example/productivityapp/Points.java
+++ app/src/main/java/com/example/productivityapp/Points.java
@@ -1,4 +1,21 @@
 package com.example.productivityapp;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity (tableName = "points")
 public class Points {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "points")
+    public int points;
+
+    public Points(@NonNull int points) {this.points = points;}
+
+    public int getPoints(){
+        return this.points;
+    }
 }
