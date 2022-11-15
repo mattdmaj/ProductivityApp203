740f19129fcbaacd7d3c17e05b882aeab61bfff6
Owlss <eerinmacdonaldd@gmail.com>
2022-11-15 17:28:29 -0500

    added prototype inventory screen with no functionalty

--- app/src/main/java/com/example/productivityapp/InventoryScreen.java
+++ app/src/main/java/com/example/productivityapp/InventoryScreen.java
@@ -10,19 +10,11 @@ import android.widget.Button;
 import com.example.productivityapp.databinding.ActivityMainBinding;
 
 public class InventoryScreen extends AppCompatActivity {
-    Boolean[] owned = new Boolean[6];
-    int selection = 0;
-
-    private ActivityMainBinding binding;
-
+    boolean[] own = {true,false,false,false,false,false};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory_screen);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
         Button base = findViewById(R.id.base_button);
         base.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,37 +22,60 @@ public class InventoryScreen extends AppCompatActivity {
 
             }
         });
+
         Button swim = findViewById(R.id.swim_button);
         swim.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(own[1] == false){
+                    
+                } else {
 
+                }
             }
         });
         Button doc = findViewById(R.id.doc_button);
         doc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(own[2] == false){
 
+                } else {
+
+                }
             }
         });
         Button gang = findViewById(R.id.gang_button);
         gang.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(own[3] == false){
+
+                } else {
+
+                }
             }
         });
         Button nes = findViewById(R.id.nes_button);
         nes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(own[4] == false){
+
+                } else {
+
+                }
             }
         });
         Button school = findViewById(R.id.school_button);
         school.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(own[5] == false){
 
+                } else {
+
+                }
             }
         });
     }
