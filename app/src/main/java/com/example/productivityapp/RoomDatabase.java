0627104a243b27c3b8e96f27e83e91c4b15c6923
Paul Lamy <lamywmpaul@gmail.com>
2022-11-17 17:38:46 -0500

    removing the PointsRoomDatabase file
    doing this in favor of having only One database file

--- app/src/main/java/com/example/productivityapp/RoomDatabase.java
+++ app/src/main/java/com/example/productivityapp/RoomDatabase.java
@@ -1,9 +1,9 @@
 package com.example.productivityapp;
 
 import androidx.room.Database;
-import androidx.room.RoomDatabase;
 
 @Database(entities = {Points.class}, version = 1, exportSchema = false)
-public abstract class PointsRoomDatabase extends RoomDatabase {
+public abstract class RoomDatabase extends androidx.room.RoomDatabase {
+    public abstract PointsDao pointsDao();
 
 }
