d9f967a2242bbf62929c80ab0bd53935afda473a
Matthew Majchrzak <mdm02nh@gmail.com>
2022-12-01 17:16:14 -0500

    Add popup screen for task scheduling and added push notifications when a task is scheduled

--- app/src/main/java/com/example/productivityapp/CalendarActivity.java
+++ app/src/main/java/com/example/productivityapp/CalendarActivity.java
@@ -14,13 +14,12 @@ public class CalendarActivity extends MainActivity {
         setContentView(R.layout.activity_main);
 
         calendarView = findViewById(R.id.calendar);
-        textView = findViewById(R.id.textView3);
+        textView = findViewById(R.id.date_view_field);
 
         new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int day)
             {
-
                 String Date = (month + 1) + "/" + day + "/" + year;
                 textView.setText(Date);
             }
