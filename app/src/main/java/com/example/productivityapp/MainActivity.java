310dd20e091b32e86999414fa6d485e19de74a58
Amra Ibrahimovic <amra.ibrahimovic@snhu.edu>
2022-12-06 16:56:10 -0500

    Make Dark Mode save when app restarts.

--- app/src/main/java/com/example/productivityapp/MainActivity.java
+++ app/src/main/java/com/example/productivityapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.productivityapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +9,7 @@ import android.widget.Button;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,6 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.PopupMenu;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.example.productivityapp.databinding.ActivityMainBinding;
@@ -33,6 +36,15 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        SharedPreferences switchPref = getSharedPreferences("save2", MODE_PRIVATE);
+        boolean isNightMode = switchPref.getBoolean("Night",true);
+        if (isNightMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
--- app/src/main/java/com/example/productivityapp/SettingsScreen.java
+++ app/src/main/java/com/example/productivityapp/SettingsScreen.java
@@ -2,14 +2,17 @@ package com.example.productivityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -25,11 +28,13 @@ import android.widget.ToggleButton;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.Calendar;
+
 public class SettingsScreen extends AppCompatActivity {
 
     ConstraintLayout constraintLayout;
     ToggleButton toggleButton;
-    Switch NightSwitch;
+    Switch nightSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,16 +43,52 @@ public class SettingsScreen extends AppCompatActivity {
 
         constraintLayout = findViewById(R.id.constraintLayout);
         toggleButton = findViewById(R.id.toggleButton);
-        NightSwitch = findViewById(R.id.NightSwitch);
+        nightSwitch = findViewById(R.id.nightSwitch);
 
+
+        //Save state of toggle button
+        SharedPreferences togglePref = getSharedPreferences("save",MODE_PRIVATE);
+        toggleButton.setChecked(togglePref.getBoolean("value", true));
+
+        //Save state of switch button
+        SharedPreferences switchPref = getSharedPreferences("save2", MODE_PRIVATE);
+        nightSwitch.setChecked(switchPref.getBoolean("Night",true));
+
+        nightSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (nightSwitch.isChecked())
+                {
+                    // When switch checked
+                    SharedPreferences.Editor editor=getSharedPreferences("save2",MODE_PRIVATE).edit();
+                    editor.putBoolean("Night",true);
+                    editor.apply();
+                    nightSwitch.setChecked(true);
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+                else
+                {
+                    // When switch unchecked
+                    SharedPreferences.Editor editor=getSharedPreferences("save2",MODE_PRIVATE).edit();
+                    editor.putBoolean("Night",false);
+                    editor.apply();
+                    nightSwitch.setChecked(false);
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+
+
+        //notification code
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
         }
-
-        SharedPreferences sharedPreferences = getSharedPreferences("save",MODE_PRIVATE);
-        toggleButton.setChecked(sharedPreferences.getBoolean("value", true));
+        //Intent for Opening App when push notif clicked
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
 
         //This is the "about us" button which will bring the user to a new screen.
         ImageButton imageButton = findViewById(R.id.Infobutton);
@@ -69,7 +110,9 @@ public class SettingsScreen extends AppCompatActivity {
                     builder.setContentTitle("Productivity App");
                     builder.setContentText("Long time no see, come study soon :D ");
                     builder.setSmallIcon(R.drawable.ic_notifications_black_24dp);
+                    builder.setContentIntent(pendingIntent);
                     builder.setAutoCancel(true);
+
 
                     NotificationManagerCompat managerCompat = NotificationManagerCompat.from(SettingsScreen.this);
                     managerCompat.notify(1,builder.build());
@@ -89,20 +132,6 @@ public class SettingsScreen extends AppCompatActivity {
                     showSnackBar2();
                 }
 
-            }
-        });
-
-        NightSwitch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (NightSwitch.isChecked()) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    NightSwitch.setChecked(true);
-                }
-                else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    NightSwitch.setChecked(false);
-                }
             }
         });
 
